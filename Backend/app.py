945d6f654561509e822aa3a11810cde583d7b64b
Heitor Galindo Gonçalves <heitorgalindo@MacBook-Air-de-Heitor.local>
2025-06-17 20:52:59 -0300

    Corrigindo porta para Railway

--- Backend/app.py
+++ Backend/app.py
@@ -138,4 +138,5 @@ if __name__ != "__main__":
     gunicorn_app = app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
